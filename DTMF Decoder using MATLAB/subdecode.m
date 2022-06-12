6aa08e6b1eb93625e686a8302832a595f2f2ef1f
inder-singh-20 <81145378+inder-singh-20@users.noreply.github.com>
2022-06-12 22:13:29 +0530

    commit

---
+++ DTMF Decoder using MATLAB/subdecode.m
@@ -0,0 +1,102 @@
+axes(handles.fig1);
+plot(t,y);
+set(handles.fig1,'XMinorTick','on');
+title('DTMF Input');
+ylabel('Amplitude');
+grid;
+
+rmain=2048*2;
+rmag=1024*2;
+
+cn=9; %order of filter
+cr=0.5; % peak to peak passband ripples
+cl=.25; % normalized passband frequency
+ch=.28; % normalized passband frequency
+
+[b,a]=cheby1(cn,cr,cl); % chebyshev type 1 filter
+yfilt1=filter(b,a,y);
+h2=fft(yfilt1,rmain);
+hmag2=abs(h2(1:rmag));
+
+[b1,a1]=cheby1(cn,cr,ch,'high');
+yfilt2=filter(b1,a1,y);
+h3=fft(yfilt2,rmain);
+hmag3=abs(h3(1:rmag));
+
+% 2nd figure
+axes(handles.fig2);
+plot(yfilt1);
+title('Filtered Low Freq. Signal');
+ylabel('Amplitude');
+grid;
+
+% 3rd figure
+axes(handles.fig3);
+plot(yfilt2);
+title('Filtered High Freq. Signal');
+xlabel('Time');
+ylabel('Amplitude');
+grid;
+
+% 4th figure
+hlow=fft(yfilt1,rmain);
+hmaglow=abs(hlow);
+
+axes(handles.fig4);
+plot(hmaglow(1:rmag));
+title('FFT Low Pass');
+ylabel('Amplitude');
+grid;
+
+% 5th figure
+hhigh=fft(yfilt2,rmain);
+hmaghigh=abs(hhigh);
+
+axes(handles.fig5);
+plot(hmaghigh(1:rmag));
+title('FFT High Pass');
+xlabel('Time');
+ylabel('Amplitude');
+grid;
+
+m=max(abs(hmag2));
+n=max(abs(hmag3));
+o=find(m==hmag2);
+p=find(n==hmag3);
+
+j=((o-1)*fs)/rmain;
+k=((p-1)*fs)/rmain;
+
+if j<=732.59 & k<=1270.91;
+   disp('---> Key Pressed is 1');
+   elseif j<=732.59 & k<=1404.73;
+      disp('---> Key Pressed is 2');
+   elseif j<=732.59 & k<=1553.04;
+      disp('---> Key Pressed is 3');
+   elseif j<=732.59 & k>1553.05;
+      disp('---> Key Pressed is A');
+   elseif j<=809.96 & k<=1270.91;   
+      disp('---> Key Pressed is 4');
+   elseif j<=809.96 & k<=1404.73;
+      disp('---> Key Pressed is 5');
+   elseif j<=809.96 & k<=1553.04;
+      disp('---> Key Pressed is 6');   
+   elseif j<=809.96 & k>1553.05;
+      disp('---> Key Pressed is B');  
+   elseif j<=895.39 & k<=1270.91;
+      disp('---> Key Pressed is 7');
+   elseif j<=895.39 & k<=1404.73;
+      disp('---> Key Pressed is 8');
+   elseif j<=895.39 & k<=1553.04;
+      disp('---> Key Pressed is 9');
+   elseif j<=895.39 & k>1553.05;      
+      disp('---> Key Pressed is C');   
+   elseif j>895.40 & k<=1270.91;   
+      disp('---> Key Pressed is *');
+   elseif j>895.40 & k<=1404.73;  
+      disp('---> Key Pressed is 0');
+   elseif j>895.40 & k<=1553.04;  
+      disp('---> Key Pressed is #');
+   elseif j>895.40 & k>1553.05;  
+      disp('---> Key Pressed is D');
+end
